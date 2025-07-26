60fbf15ef35f14422d5355dc72ce651f0c2430ae
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-26 11:04:04 +0800

    release file name

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
-
-applicationVariants.all {
-    val variant = this
-    variant.outputs
-        .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
-        .filter {
-            val names = it.name.split("-")
-            it.name.lowercase().contains(names[0], true) && it.name.lowercase().contains(names[1], true)
-        }
-        .forEach { output ->
-            val outputFileName = "fqhll_keyboard.apk"
-            output.outputFileName = outputFileName
-        }
-}
