1c03ae505f85ed22cbb8e21439a15cb9c620b3f4
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-26 08:26:57 +0800

    working keyboard prototype

--- app/src/main/java/com/fqhll/keyboard/CustomKeyboardApp.java
+++ app/src/main/java/com/fqhll/keyboard/CustomKeyboardApp.java
@@ -1,4 +1,80 @@
 package com.fqhll.keyboard;
 
-public class CustomKeyboardApp {
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class CustomKeyboardApp extends InputMethodService
+        implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+
+    @Override
+    public View onCreateInputView() {
+        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.custom_keyboard_layout, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.custom_keypad);
+        keyboardView.setKeyboard(keyboard);
+
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+
+    @Override
+    public void onPress(int i) {
+    }
+
+    @Override
+    public void onRelease(int i) {
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && caps){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+    }
+
+    @Override
+    public void swipeLeft() {
+    }
+
+    @Override
+    public void swipeRight() {
+    }
+
+    @Override
+    public void swipeDown() {
+    }
+
+    @Override
+    public void swipeUp() {
+    }
 }
