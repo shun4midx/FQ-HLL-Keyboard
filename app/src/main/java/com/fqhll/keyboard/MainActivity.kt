80d67a19346bdd4519445dac9b9b48bc26531231
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-31 23:09:44 +0800

    github repo link

--- app/src/main/java/com/fqhll/keyboard/MainActivity.kt
+++ app/src/main/java/com/fqhll/keyboard/MainActivity.kt
@@ -2,10 +2,12 @@ package com.fqhll.keyboard
 
 import android.content.Context
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
@@ -21,6 +23,9 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repoLink: TextView = findViewById(R.id.repoLink)
+        repoLink.movementMethod = LinkMovementMethod.getInstance()
 
         val capsToggle: SwitchCompat = findViewById(R.id.capsToggle)
         val autocorToggle: SwitchCompat = findViewById(R.id.autocorToggle)
