e1d31b89b8063ea0331ae175903b866a9518cefe
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-08-21 18:16:23 +0800

    oiiai

--- app/src/main/java/com/fqhll/keyboard/MainActivity.kt
+++ app/src/main/java/com/fqhll/keyboard/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private var keyboardHeights = arrayOf("Unselected", "Short", "Medium", "Tall", "Custom")
     private var keyboardLayouts = arrayOf("Unselected", "QWERTY", "QWERTZ", "AZERTY", "Dvorak", "Colemak", "Zhuyin")
     private var emojiVariations = arrayOf("Unselected", "Masculine", "Feminine", "Neutral")
-    private var keySound = arrayOf("Unselected", "click", "meow", "quack")
+    private var keySound = arrayOf("Unselected", "click", "meow", "quack", "oiiai")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
